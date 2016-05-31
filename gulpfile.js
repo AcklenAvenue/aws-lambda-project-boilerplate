388788d8672bdc87e2f7cd8f26a0f6142cc27ad8
Osman Hernandez <osman.sist@gmail.com>
2016-05-31 16:13:28 -0600

    lamba function now been created from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,100 +7,74 @@ var zip = require('gulp-zip');
 var AWS = require('aws-sdk');
 var fs = require('fs');
 var runSequence = require('run-sequence');
+var newer = require('gulp-newer');
+var tsc = require('gulp-typescript');
+var debug = require('gulp-debug');
+
+var compileTypescript = function(srcPath) {
+    return gulp.src([srcPath + '/**/*.ts', '!' + srcPath + '/**/*.d.ts'])
+        .pipe(newer({
+            dest: './dist',
+            ext: '.js'
+        }))
+        .pipe(tsc())
+        .pipe(gulp.dest('./dist'));
+};
+
+gulp.task('compile-src', function() {
+    return compileTypescript('src');
+});
 
 // First we need to clean out the dist folder and remove the compiled zip file.
 gulp.task('clean', function(cb) {
-  del('./dist',
-    del('./archive.zip', cb)
-  );
-});
-
-// The js task could be replaced with gulp-coffee as desired.
-gulp.task('js', function() {
-  gulp.src('index.js')
-    .pipe(gulp.dest('dist/'))
+    return del('./dist',
+        del('./archive.zip', cb)
+    );
 });
 
 // Here we want to install npm packages to dist, ignoring devDependencies.
 gulp.task('npm', function() {
-  gulp.src('./package.json')
-    .pipe(gulp.dest('./dist/'))
-    .pipe(install({production: true}));
-});
-
-// Next copy over environment variables managed outside of source control.
-gulp.task('env', function() {
-  gulp.src('./config.env.production')
-    .pipe(rename('.env'))
-    .pipe(gulp.dest('./dist'))
+    return gulp.src('./package.json')
+        .pipe(gulp.dest('./dist/greeter/'))
+        .pipe(install({
+            production: true
+        }));
 });
 
 // Now the dist directory is ready to go. Zip it.
 gulp.task('zip', function() {
-  gulp.src(['dist/**/*', '!dist/package.json', 'dist/.*'])
-    .pipe(zip('dist.zip'))
-    .pipe(gulp.dest('./'));
+    return gulp.src(['dist/greeter/**/*', '!dist/package.json'])
+        .pipe(zip('dist.zip'))
+        .pipe(gulp.dest('./'));
 });
 
-// Per the gulp guidelines, we do not need a plugin for something that can be
-// done easily with an existing node module. #CodeOverConfig
-//
-// Note: This presumes that AWS.config already has credentials. This will be
-// the case if you have installed and configured the AWS CLI.
-//
-// See http://aws.amazon.com/sdk-for-node-js/
-gulp.task('upload', function() {
-
-  // TODO: This should probably pull from package.json
-  AWS.config.region = 'us-east-1';
-  var lambda = new AWS.Lambda();
-  var functionName = 'hello-world';
-
-  // lambda.getFunction({FunctionName: functionName}, function(err, data) {
-  //   if (err) {
-  //     if (err.statusCode === 404) {
-  //       var warning = 'Unable to find lambda function ' + deploy_function + '. '
-  //       warning += 'Verify the lambda function name and AWS region are correct.'
-  //       gutil.log(warning);
-  //     } else {
-  //       var warning = 'AWS API request failed. '
-  //       warning += 'Check your AWS credentials and permissions.'
-  //       gutil.log(warning);
-  //     }
-  //   }
-
-    // This is a bit silly, simply because these five parameters are required.
-    //var current = data.Configuration;
-    // console.log(current);
+gulp.task('upload', function(done) {
+    AWS.config.region = 'us-east-1';
+    var lambda = new AWS.Lambda();
+    var functionName = 'hello-world';
 
     var params = {
-      FunctionName: functionName,
-      Handler: 'index.handler',
-      //Mode: current.Mode,
-      Role: 'arn:aws:iam::487799950875:role/lambda_basic_execution',
-      Runtime: 'nodejs4.3'
+        FunctionName: functionName,
+        Handler: 'handler.handler',
+        Role: 'arn:aws:iam::487799950875:role/lambda_basic_execution',
+        Runtime: 'nodejs4.3',
+        Code: {}
     };
 
     fs.readFile('./dist.zip', function(err, data) {
-      params['FunctionZip'] = data;
-      lambda.createFunction(params, function(err, data) {
-        if (err) {
-          var warning = 'Package upload failed. '
-          warning += 'Check your iam:PassRole permissions.'
-          gutil.log(warning);
-        }
-      });
+        params.Code['ZipFile'] = data;
+        lambda.createFunction(params, function(err, data) {
+            if (err) {
+                console.log(err);
+            }
+            done();
+        });
     });
-  //});
 });
 
-// The key to deploying as a single command is to manage the sequence of events.
 gulp.task('default', function(callback) {
-  return runSequence(
-    ['clean'],
-    ['js', 'npm', 'env'],
-    ['zip'],
-    ['upload'],
-    callback
-  );
+    return runSequence(
+        ['clean'], ['compile-src'], ['npm'], ['zip'], ['upload'],
+        callback
+    );
 });
