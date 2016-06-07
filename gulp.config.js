a6f4b41e7cc84e142aee754079f435505610d933
Richard Siwady <rsiwady29@gmail.com>
2016-06-07 09:11:27 -0600

    Using . synthax instead of []

--- gulp.config.js
+++ gulp.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    awsAccessKeyId: process.env["awsAccessKeyId"],
-    awsSecretAccessKey: process.env["awsSecretAccessKey"],
-    awsAccountNumber: process.env["awsAccountNumber"],
-    awsRegion: process.env["awsRegion"],
-    lambdaRole: process.env["lambdaRole"]
+    awsAccessKeyId: process.env.awsAccessKeyId,
+    awsSecretAccessKey: process.env.awsSecretAccessKey.,
+    awsAccountNumber: process.env.awsAccountNumber,
+    awsRegion: process.env.awsRegion,
+    lambdaRole: process.env.lambdaRole
 };
