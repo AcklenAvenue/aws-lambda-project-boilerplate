9b6708b0b0b0fb75c4f20eeb0167381ec887d9fd
Richard Siwady <rsiwady29@gmail.com>
2016-06-07 21:42:28 -0600

    Created appveyor.yml

--- gulp.config.js
+++ gulp.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     awsAccessKeyId: process.env.awsAccessKeyId,
-    awsSecretAccessKey: process.env.awsSecretAccessKey.,
+    awsSecretAccessKey: process.env.awsSecretAccessKey,
     awsAccountNumber: process.env.awsAccountNumber,
     awsRegion: process.env.awsRegion,
     lambdaRole: process.env.lambdaRole
