3cb487d3040747f36ffa0bc9df2453af5c3bb984
Camilo Rivera <camilo@riverapineda.com>
2018-09-06 11:38:30 -0600

    setting up lambda with serverless and lerna

--- gulp.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    awsAccessKeyId: process.env.awsAccessKeyId,
-    awsSecretAccessKey: process.env.awsSecretAccessKey,
-    awsAccountNumber: process.env.awsAccountNumber,
-    awsRegion: process.env.awsRegion,
-    lambdaRole: process.env.lambdaRole
-};
