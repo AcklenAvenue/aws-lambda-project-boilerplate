fbacff152af32e7eb85d5c209af70cc607de51e2
Camilo Rivera <camilo@riverapineda.com>
2018-09-21 05:42:34 +0800

    config env creds

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 set -eo pipefail
+echo "--- PRINT ENV"
+env
 echo "--- Deploy to $BUILDKITE_BRANCH"
 cd service
+if [[ "$BUILDKITE_BRANCH" == "develop" ]]; then
+  serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
+fi
+if [[ "$BUILDKITE_BRANCH" == "staging" ]]; then
+  serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
+fi
+if [[ "$BUILDKITE_BRANCH" == "master" ]]; then
+  serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID_PROD --secret $AWS_SECRET_ACCESS_KEY_PROD
+fi
 serverless deploy --stage $BUILDKITE_BRANCH
